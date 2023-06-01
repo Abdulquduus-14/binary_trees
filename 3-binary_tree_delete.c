77dc8e97baef0ea9d554f09d474a8bbfa782d85a
Amidu Qudus <qudusamidu14@gmail.com>
2023-05-31 23:10:30 -0700

    tsk3 rechecked

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,12 +7,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return;
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
-		free(tree);
+		/* free(tree); */
 		binary_tree_delete(tree->right);
+		free(tree);
 	}
 }
